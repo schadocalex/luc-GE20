70cb5935863b19ec4cf60eb6a36e5add17c5cd03
Alexis Schad <schadoc_alex@hotmail.fr>
2015-01-04 14:44:22 +0100

    taille image

--- carter_gauche.php
+++ carter_gauche.php
@@ -8,44 +8,44 @@
 	</object>
 </div>
 
-<p>
+<div>
 	<br/><br/>Le surfacique :
 	<br/><br/>Dans un premier temps on colle en collage spéciale, en tant que résultat avec lien, donc ici on prend tout le squelette pour avoir une idée de l’encombrement.
-	<br/><img src="img/6.png" class="full-width" />
+	<br/><p class="center" ><img src="img/6.png" class="img_size" /></p>
 	<br/><br/>Ensuite il nous faut créer toutes les surfaces de notre carter :
 	<br/><br/>On ne se placera pas directement sur le squelette car il y aura une épaisseur par la suite, on fait donc une esquisse comme cela sur le plan zx.
-	<br/><img src="img/7.png" class="full-width" />
+	<br/><p class="center" ><img src="img/7.png" class="img_size" /></p>
 	<br/><br/>On vient rajouter 2 esquisses et leurs extrusions toujours dans le plan ZX. Pour faire les 2 bords de la ponceuse.
-	<br/><img src="img/8.png" class="full-width" />
+	<br/><p class="center" ><img src="img/8.png" class="img_size" /></p>
 	<br/><br/>De même pour le haut et le bas de la poignée :
-	<br/><img src="img/9.png" class="full-width" />
+	<br/><p class="center" ><img src="img/9.png" class="img_size" /></p>
 	<br/><br/>On vient fermer le coter et le bout devant, toujours selon le même principe : esquisse, extrusion
-	<br/><img src="img/10.png" class="full-width" />
+	<br/><p class="center" ><img src="img/10.png" class="img_size" /></p>
 	<br/><br/>On fait pareil à l’arriere
-	<br/><img src="img/11.png" class="full-width" />
+	<br/><p class="center" ><img src="img/11.png" class="img_size" /></p>
 	<br/><br/>On relimite tout ça en un coup :
-	<br/><img src="img/12.png" class="full-width" />
+	<br/><p class="center" ><img src="img/12.png" class="img_size" /></p>
 	<br/><br/>On fait le bout de la poignée avec une révolution, puis on va remplir de meme à l’avant :
-	<br/><img src="img/13.png" class="full-width" />
+	<br/><p class="center" ><img src="img/13.png" class="img_size" /></p>
 	<br/><br/>Maintenant on va remplir tout le devant avec soit l’outil remplissage soit en faisant de nouvelles esquisses et on relimite :
-	<br/><img src="img/14.png" class="full-width" />
+	<br/><p class="center" ><img src="img/14.png" class="img_size" /></p>
 	<br/><br/>Il reste donc l’arriere à faire, avec une petite esquisse/extrusion, et un cone en revolution tout au bout:
-	<br/><img src="img/15.png" class="full-width" />
+	<br/><p class="center" ><img src="img/15.png" class="img_size" /></p>
 	<br/><br/>Reste enfin a relimiter tout ça, découper ce qui dépasserait, et enfin assembler pour obtenir :
-	<br/><img src="img/16.png" class="full-width" />
+	<br/><p class="center" ><img src="img/16.png" class="img_size" /></p>
 	<br/><br/>Ainsi nous avons la forme extérieure, on vérifie bien que tout le squelette est contenu dans le carter, pour éviter les interférences.
 	<br/><br/>Maintenant place au volumique, on va passer rapidement dessus, car c’est juste long et répétitif…
 	<br/><br/>On va donc dans un premier temps épaissir tout ça, faire tous les congés éventuels pour arrondir les angles comme sur notre modèle réel :
-	<br/><img src="img/17.png" class="full-width" />
+	<br/><p class="center" ><img src="img/17.png" class="img_size" /></p>
 	<br/><br/>On va ensuite faire les trous princpaux et l’extrusion de la partie permettant le guidage de l’arbre :
-	<br/><img src="img/18.png" class="full-width" />
+	<br/><p class="center" ><img src="img/18.png" class="img_size" /></p>
 	<br/><br/>On va ensuite faire toutes les petites nervures du bas :
-	<br/><img src="img/19.png" class="full-width" />
+	<br/><p class="center" ><img src="img/19.png" class="img_size" /></p>
 	<br/><br/>Ensuite on va faire à coup d’extrusion, poche, trous les différents éléments du haut :
-	<br/><img src="img/20.png" class="full-width" />
+	<br/><p class="center" ><img src="img/20.png" class="img_size" /></p>
 	<br/><br/>Ensuite il faut faire toutes les nervures, on utilisera pas mal l’outil répétition rectangulaire et la symétrie :
-	<br/><img src="img/21.png" class="full-width" />
+	<br/><p class="center" ><img src="img/21.png" class="img_size" /></p>
 	<br/><br/>reste ensuite à rajouter les petits détails, rien de bien compliqué, pas mal d’extrusion, poches et nervures, pour obtenir le résultat final :
-	<br/><img src="img/22.png" class="full-width" />
-</p>
+	<br/><p class="center" ><img src="img/22.png" class="img_size" /></p>
+</div>
 <?php include("footer.php"); ?>
